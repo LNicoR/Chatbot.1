fd890ea8e07572884a01ea68fff2513cf1925206
Carlos Diaz <carlos97dt@gmail.com>
2024-11-05 21:17:18 -0500

    All origins

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,6 +2,6 @@ from typing import List
 
 class Settings:
     PROJECT_NAME: str = "Chatbot API"
-    ALLOWED_ORIGINS: List[str] = ["https://c-bot-ui.onrender.com", "http://localhost"]
+    ALLOWED_ORIGINS: List[str] = ["*"]
 
 settings = Settings()
