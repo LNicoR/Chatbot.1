a1ffa65fbaa69320b4021304e89958476d9910db
Carlos Diaz <carlos97dt@gmail.com>
2024-11-06 11:14:50 -0500

    Origins in config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,6 +2,10 @@ from typing import List
 
 class Settings:
     PROJECT_NAME: str = "Chatbot API"
-    ALLOWED_ORIGINS: List[str] = ["https://cbot-lemon.vercel.app/", "http:localhost:3000"]
+    ALLOWED_ORIGINS: List[str] = [
+        "http://localhost:3000",
+        "https://cbot-lemon.vercel.app",
+        "https://cbot-git-main-c4rlos9559s-projects.vercel.app",
+        "https://cbot-6kgnaeb9m-c4rlos9559s-projects.vercel.app",]
 
 settings = Settings()
