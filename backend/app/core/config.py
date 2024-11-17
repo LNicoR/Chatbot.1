f21fefed187b515c8cd6ab724a52cb26ed1b9747
lnicor <nico.0402linares@outlook.es>
2024-11-17 18:20:42 -0500

    acordarse

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,7 +4,6 @@ class Settings:
     PROJECT_NAME: str = "Chatbot API"
     ALLOWED_ORIGINS: List[str] = [
         "http://localhost:3000",
-        "https://cbot-lemon.vercel.app",
-        "https://cbot-ui.vercel.app",
+
     ]
 settings = Settings()
